1a05bfa7d067f8fddce81ed577ddd2055aca3915
SarahAlmutairi <76258593+SarahAlmutairi@users.noreply.github.com>
2021-01-09 14:41:59 +0300

    Initial commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Sam Al-Ahmed on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
